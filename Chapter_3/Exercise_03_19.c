fe86bbc9ab0cba9ad81a91039502eeff4b42e668
Jason Clare <70923049+mintyModulo@users.noreply.github.com>
2023-09-21 19:50:58 -0500

    Finished

--- Chapter_3/Exercise_03_19.c
+++ Chapter_3/Exercise_03_19.c
@@ -1,27 +1,33 @@
-// One large chemical company pays its salespeople on a commision basis. The salespeople receive $200 per week plus 9% of their gross sales for that week.
-// For example, a salesperson who sells $5000 worth of chemicals in a week receives $200 plus 9% of $5000, or a total of $650.
-// Develop a program that will input each salesperson's gross sales for last week and will calculate and display that salesperson's earnings.
-// Process one salesperson's figures at a time.
+// The simple interest on a loan is calculated by the formula
+//      interest = principal * rate * days / 365
+// The preceding formula assumes that rate is the annual interest rate, and therefore includes the division by 365 (days).
+// Develop a program that will input principal, rate and days for several loans, and will calculate and display the simple interest for each loan, using the preceding formula.
 
 #include <stdio.h>
 
-int main(void)
+int main (void)
 {
-    float sales;
-    float setPay = 200.00;
+    float principal;
+    float rate;
+    unsigned int days;
 
-    printf("%s", "Enter sales in dollars (-1 to end): ");
-    scanf("%f", &sales);
-    
-    // loop until user ends
-    while (sales != -1) {
-        float totalPay = (sales * .09) + setPay;
+    printf("%s", "Enter loan principal (-1 to end): ");
+    scanf("%f", &principal);
 
-        printf("Salary is: $%.2f\n", totalPay);
+    while (principal != -1) {
+        printf("%s", "Enter interest rate: ");
+        scanf("%f", &rate);
 
-        printf("%s", "\nEnter sales in dollars (-1 to end): ");
-        scanf("%f", &sales);
-    } // end while
+        printf("Enter term of the loan in days: ");
+        scanf("%u", &days);
+
+        float interest = principal * rate * days / 365;
+
+        printf("The interest charge is $%.2f\n", interest);
+
+        printf("%s", "\nEnter loan principal (-1 to end): ");
+        scanf("%f", &principal);
+    }
 
     return 0;
 }
